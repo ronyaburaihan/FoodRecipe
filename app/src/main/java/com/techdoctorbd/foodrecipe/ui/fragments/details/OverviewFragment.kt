1048b3a3ac6aa3e28da8c7f5f95ac6eb241352a2
Abu Raihan Rony <44436797+ronyaburaihan@users.noreply.github.com>
2021-06-15 21:22:18 +0600

    Recipe overview added

--- app/src/main/java/com/techdoctorbd/foodrecipe/ui/fragments/details/OverviewFragment.kt
+++ app/src/main/java/com/techdoctorbd/foodrecipe/ui/fragments/details/OverviewFragment.kt
@@ -1,20 +1,133 @@
 package com.techdoctorbd.foodrecipe.ui.fragments.details
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.techdoctorbd.foodrecipe.R
+import com.techdoctorbd.foodrecipe.data.models.Result
+import com.techdoctorbd.foodrecipe.databinding.FragmentOverviewBinding
+import com.techdoctorbd.foodrecipe.utils.loadImageFromUrl
 
 
 class OverviewFragment : Fragment() {
 
+    private var _binding: FragmentOverviewBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        _binding = FragmentOverviewBinding.inflate(inflater, container, false)
+
+        val args = arguments
+        val myBundle: Result = args?.getSerializable("recipeBundle") as Result
+
+        binding.mainImage.loadImageFromUrl(myBundle.image)
+        binding.tvTitle.text = myBundle.title
+        binding.tvLike.text = myBundle.aggregateLikes.toString()
+        binding.tvTime.text = myBundle.readyInMinutes.toString()
+        binding.tvSummery.text = myBundle.summary
+
+        if (myBundle.vegetarian) {
+            binding.ivVegetarian.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.tvVegetarian.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle.vegan) {
+            binding.ivVegan.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.tvVegan.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle.glutenFree) {
+            binding.ivGlutenFree.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.tvGlutenFree.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle.dairyFree) {
+            binding.ivDairyFree.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.tvDairyFree.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle.veryHealthy) {
+            binding.ivHealthy.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.tvHealthy.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        if (myBundle.cheap) {
+            binding.ivCheap.setColorFilter(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+            binding.tvCheap.setTextColor(
+                ContextCompat.getColor(
+                    requireContext(),
+                    R.color.green
+                )
+            )
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/com/techdoctorbd/foodrecipe/utils/Extentions.kt
+++ app/src/main/java/com/techdoctorbd/foodrecipe/utils/Extentions.kt
@@ -1,8 +1,11 @@
 package com.techdoctorbd.foodrecipe.utils
 
+import android.widget.ImageView
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import coil.load
+import com.techdoctorbd.foodrecipe.R
 
 fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observer<T>) {
     observe(lifecycleOwner, object : Observer<T> {
@@ -11,4 +14,10 @@ fun <T> LiveData<T>.observeOnce(lifecycleOwner: LifecycleOwner, observer: Observ
             observer.onChanged(t)
         }
     })
+}
+
+fun ImageView.loadImageFromUrl(url: String) = this.load(url) {
+    crossfade(true)
+    crossfade(600)
+    error(R.drawable.ic_error_placeholder)
 }
